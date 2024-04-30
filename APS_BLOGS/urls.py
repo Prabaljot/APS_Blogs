6ad07b75b616757221551609625948dfe2095948
Prabaljot <jotprabal5@gmail.com>
2024-04-30 17:09:33 +0530

    added thumbnail icon on blog home

--- APS_BLOGS/urls.py
+++ APS_BLOGS/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('add_blog/', admin.site.urls),
     path('',include('Home.urls')),
     path('blog/',include('Blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
