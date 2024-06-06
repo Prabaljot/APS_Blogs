5cd64b519126a5c3e1dbd561ba3bd79650f0a885
Prabaljot <jotprabal5@gmail.com>
2024-06-06 12:03:59 +0530

    autoslug added

--- Blog/models.py
+++ Blog/models.py
@@ -1,30 +1,15 @@
 from django.db import models
 from datetime import datetime
+from autoslug import AutoSlugField
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Post(models.Model):
-    category_choices = (
-        ('general', 'General'),
-        ('science', 'Science'),
-        ('english', 'English'),
-        ('maths', 'Maths'),
-        ('sst', 'SST'),
-        ('gaming', 'Gaming'),
-        ('news', 'News'),
-        ('announcements', 'Announcements'),
-        ('sports', 'Sports'),
-        ('cooking', 'Cooking'),
-        ('lifestyle', 'Lifestyle'),
-        ('news', 'News'),
-    )
     sno = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     text = models.TextField()
     author = models.CharField(max_length=30)
-    slug = models.CharField(max_length=300)
+    slug = AutoSlugField(populate_from='title')
     timeStamp = models.DateTimeField(default=datetime.now, blank=True)
     thumbnail = models.ImageField(upload_to='blog/thumbnail', default="blog/thumbnail/wallpaper.jpg")
 
